3be5bd3af66001b213e34667c3187ae363d61c76
Agetedo <114358496+Agetedo@users.noreply.github.com>
2024-03-07 00:35:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Page404 from "./pages/Page404";
 export default function App() {
   const router = createBrowserRouter([
     {
+      path: '/',
       element: <Layout />,
       errorElement: <Page404 />,
       children: routes,
